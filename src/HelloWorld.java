84dc7bbdb76beb4daf2a610b9728ea41bc4cb7e1
antoniooreany <antonio_oreany@mail.ru>
2016-09-12 20:27:04 +0300

    System.out.println("Adding for New feature!");

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
         System.out.println("Hello World!");
         System.out.println("Hello World again!");
         System.out.println("New feature!");
+        System.out.println("Adding for New feature!");
     }
 }
